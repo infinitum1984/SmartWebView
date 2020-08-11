1b04c85559521364da297906e639658e52cdb68c
Epmty <danya.nosyk@gmail.com>
2020-08-11 21:26:37 +0300

    init commit

---
+++ smartwb/src/main/java/com/empty/smartwv/SmartWV.kt
@@ -0,0 +1,10 @@
+package com.empty.smartwv
+
+import android.content.Context
+import android.webkit.WebView
+
+class SmartWV(context: Context): WebView(context) {
+    init {
+
+    }
+}
---
+++ smartwb/src/main/java/com/empty/smartwv/SmartWebChromeClient.kt
@@ -0,0 +1,4 @@
+package com.empty.smartwv
+
+class SmartWebChromeClient {
+}
